cfdb1934e62c61d40cf310a845e6e6f466298562
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-02-01 16:46:49 -0600

    Added event details components

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Link from "next/link";
-import classes from './EventItem.module.css';
+import Button from "../UI/Button";
+import classes from "./EventItem.module.css";
+import DateIcon from "../Icons/DateIcon";
+import AddressIcon from "../Icons/AddressIcon";
+import ArrowRightIcon from "../Icons/ArrowRightIcon";
 
 const EventItem = (props) => {
   const { image, title, date, location, id } = props.event;
@@ -21,14 +24,21 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{formattedDate}</time>
           </div>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedLocation}</address>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore event</Link>
+          <Button link={exploreLink}>
+            <span>Explore event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import React from "react";
+import EventContent from "../../components/EventDetail/EventContent";
+import EventLogistics from "../../components/EventDetail/EventLogistics";
+import EventSummary from "../../components/EventDetail/EventSummary";
+import { getEventById } from "../../dummy-data";
 
 const EventDetailPage = () => {
-  return <div>
-      <h1>Event Detail</h1>
-  </div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  console.log(event);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics data={event.data} address={event.location} image={event.image} imageAlt={event.imageAlt} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default EventDetailPage;
